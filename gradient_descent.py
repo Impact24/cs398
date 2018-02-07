797a66ea064014b7fc3b54beb8bea4807ecaec25
Paul Prantilla <impact24@Time-Walk.local>
2018-02-07 17:13:43 +0800

    Formatting Change

--- gradient_descent.py
+++ gradient_descent.py
@@ -27,7 +27,7 @@ def plot(third,second,first,constant,pointX):
 
 	pointY=int(third)*pointX**3+int(second)*pointX**2+int(first)*pointX+int(constant)
 	plt.annotate('Local Minimum', xy=(pointX, pointY), xytext=(pointX,pointY), arrowprops=dict(facecolor='black', shrink=0.05),) 
-	print("PointY is: %f" %pointY );
+	print("\tY:%f" %pointY );
 	plt.ylim(-50,50)
 	plt.xlim(-50,50)
 	plt.show()
@@ -39,18 +39,21 @@ f=Function('f')(x)
 cur_x = 1 # Start of algorithm
 gamma = 0.01 # step size multiplier
 precision = 0.00001 #approach a derivative of 0
+noise = 0 #noise factor
 previous_step_size = cur_x
 
 
 expression = (int(Third) * x**3) + (int(Second) * x**2) + (int(First) * x) + int(Constant) 
-df=lambdify(x,diff(expression,x),"numpy")
+df=lambdify(x,diff(expression,x)+noise,"numpy")
 
+step=0
 while previous_step_size > precision:
+    step += 1
     prev_x = cur_x
     cur_x += -gamma * df(prev_x)
     previous_step_size = abs(cur_x - prev_x)
-    print (cur_x)
-
-print("The local minimum occurs at %f with derivative %f" % (cur_x, df(cur_x)))
+    print("Step:%i\tX:%f\tDerivative:%f" %(step,cur_x,df(cur_x)))
+ 
+print("\nLocal Minimum:\n\tDerivative:%f\n\tPrecision:%f\n\tNoise:%f\n\tX:%f" % (df(cur_x),precision,noise,cur_x))
 plot(Third,Second,First,Constant,cur_x)
 
